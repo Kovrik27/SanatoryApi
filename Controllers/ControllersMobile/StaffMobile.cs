96fdbb80f6f3ae6552d1b7c6913fc147fbe1ac5b
student <student@CAB4-COMP10>
2025-10-27 13:49:58 +1000

    hfghfghfg

---
+++ Controllers/ControllersMobile/StaffMobile.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SanatoryApi.Controllers.ControllersMobile
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffMobile : ControllerBase
+    {
+
+    }
+}
---
+++ Controllers/ControllersMobile/UserMobile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SanatoryApi.Models;
+
+namespace SanatoryApi.Controllers.ControllersMobile
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserMobile : ControllerBase
+    {
+        readonly SanatoryContext db;
+
+        public UserMobile(SanatoryContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet("GetAllUsers")]
+        public async Task<List<User>> GetUsers()
+        {
+            return new List<User>(await db.Users.ToListAsync());
+        }
+    }
+}
